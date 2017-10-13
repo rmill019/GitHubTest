ff506d5fbc4fe394b9ada1dde7730a080326beb1
Ray Millares <rmill013@gmail.com>
2017-10-12 21:07:12 -0400

    Add Assignment

--- Assets/_Scripts/PrintNames.cs
+++ Assets/_Scripts/PrintNames.cs
@@ -11,21 +11,15 @@ public class PrintNames : MonoBehaviour {
 	// Please add your full name to the array under this comment. Remember to have a comma before your name and put your name in double quotes. 
 	private string [] names = {"Ray Millares"};
 
-	private StringBuilder allNames = new StringBuilder();
-
 	// Use this for initialization
 	void Start () {
 
 		_namesTextBox = GameObject.Find ("Names").GetComponent<Text>();
-
-		for (int i = 0; i < names.Length; i++)
-		{
-			allNames.Append(names[i] + ", ");
-		}
+		_namesTextBox.text = "";
 
 		foreach (string name in names)
 		{
-			_namesTextBox.text = allNames.ToString();
+			_namesTextBox.text += name + ", ";
 		}
 		
 	}
